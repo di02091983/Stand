99ba9c88202e7f15fae57132b1ac321831df2efb
Дмитрий <di1983@mail.ru>
2023-10-26 13:46:09 +0300

    1. Добавлены контроллеры для брендов, категорий и товаров. 2. Подключена база MySQL.

---
+++ Catalog/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+using Catalog.Data;
+using Catalog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        readonly ApplicationDbContext _db;
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController([FromServices] ApplicationDbContext db, ILogger<CategoryController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCategories")]
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            try
+            {
+                var categories = await _db.Categories.ToListAsync();
+                return categories;
+            }
+            catch (DbUpdateException e)
+            {
+                _logger.LogError(e, "Exception in " + this.GetType().Name + "::" + System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name);
+                return new List<Category>();
+            }
+        }
+
+        [HttpPost(Name = "AddCategory")]
+        public async Task<IActionResult> AddCategory(Category category)
+        {
+            _db.Categories.Add(category);
+
+            try
+            {
+                await _db.SaveChangesAsync();
+                return Ok();
+            }
+            catch (DbUpdateException e)
+            {
+                _logger.LogError(e, "Exception in " + this.GetType().Name + "::" + System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
---
+++ Catalog/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+using Catalog.Data;
+using Catalog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        readonly ApplicationDbContext _db;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController([FromServices] ApplicationDbContext db, ILogger<ProductController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            try
+            {
+                var products = await _db.Products.ToListAsync();
+                return products;
+            }
+            catch (DbUpdateException e)
+            {
+                _logger.LogError(e, "Exception in " + this.GetType().Name + "::" + System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name);
+                return new List<Product>();
+            }
+        }
+
+        [HttpPost(Name = "AddProduct")]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            _db.Products.Add(product);
+
+            try
+            {
+                await _db.SaveChangesAsync();
+                return Ok();
+            }
+            catch (DbUpdateException e)
+            {
+                _logger.LogError(e, "Exception in " + this.GetType().Name + "::" + System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
