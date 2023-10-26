192b14499d0f262e365deb180d1e0e3996846b66
Дмитрий <di1983@mail.ru>
2023-10-26 12:10:53 +0300

    1. Добавлены классы DTO объектов для работы с API каталога товаров.

---
+++ Gateway/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Gateway.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; } 
+
+        public string? Name { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public Guid BrandId { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
